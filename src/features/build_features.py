c65f60619ac109fa5bae79280e00e5e7092a909f
andriy59b <andriypentsak59b@gmail.com>
2025-03-31 11:34:29 +0300

    PCA / Sum of squares attributes

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -58,17 +58,66 @@ duration_df.iloc[1] / 10
 df_lowpass = df.copy()
 LowPass = LowPassFilter()
 
-fs = 1000/ 200
+fs = 1000 / 200
+cutoff = 1.2
+
+df_lowpass = LowPass.low_pass_filter(df_lowpass, "acc_y", fs, cutoff, order=5)
+
+subset = df_lowpass[df_lowpass["set"] == 45]
+print(subset["label"][0])
+
+fig, ax = plt.subplots(nrows=2, sharex=True, figsize=(20, 10))
+ax[0].plot(subset["acc_y"].reset_index(drop=True), label="raw data")
+ax[1].plot(subset["acc_y_lowpass"].reset_index(drop=True), label="butterworth filter")
+ax[0].legend(loc="upper center", bbox_to_anchor=(0.5, 1.15), fancybox=True, shadow=True)
+ax[1].legend(loc="upper center", bbox_to_anchor=(0.5, 1.15), fancybox=True, shadow=True)
+
+for col in predictor_columns:
+    df_lowpass = LowPass.low_pass_filter(df_lowpass, col, fs, cutoff, order=5)
+    df_lowpass[col] = df_lowpass[col + "_lowpass"]
+    del df_lowpass[col + "_lowpass"]
+    
 
 # --------------------------------------------------------------
 # Principal component analysis PCA
 # --------------------------------------------------------------
+
+df_pca = df_lowpass.copy()
+
+PCA = PrincipalComponentAnalysis()
+
+pc_values = PCA.determine_pc_explained_variance(df_pca, predictor_columns)
+
+plt.figure(figsize=(10, 10))
+plt.plot(range(1, len(predictor_columns) + 1), pc_values)
+plt.xlabel("principal component number")
+plt.ylabel("explained variance")
+plt.show()
+
+df_pca = PCA.apply_pca(df_pca, predictor_columns, 3)
+
+subset = df_pca[df_pca["set"] == 35]
+
+subset[["pca_1", "pca_2", "pca_3"]].plot()
 
 
 # --------------------------------------------------------------
 # Sum of squares attributes
 # --------------------------------------------------------------
 
+df_squared = df_pca.copy()
+
+acc_r = df_squared["acc_x"] ** 2 + df_squared["acc_y"] ** 2 + df_squared["acc_z"] ** 2
+gyr_r = df_squared["gyr_x"] ** 2 + df_squared["gyr_y"] ** 2 + df_squared["gyr_z"] ** 2
+
+df_squared["acc_r"] = np.sqrt(acc_r)
+df_squared["gyr_r"] = np.sqrt(gyr_r)
+
+subset = df_squared[df_squared["set"] == 14]
+
+subset[["acc_r", "gyr_r"]].plot(subplots=True)
+
+df_squared
 
 # --------------------------------------------------------------
 # Temporal abstraction
