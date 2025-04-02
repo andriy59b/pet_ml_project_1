95e5a4426ca2d45c806ea7e66630c7019d545952
andriy59b <andriypentsak59b@gmail.com>
2025-04-02 06:45:48 +0300

    Create a training and test set

---
+++ src/models/train_model.py
@@ -0,0 +1,70 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+from LearningAlgorithms import ClassificationAlgorithms
+import seaborn as sns
+import itertools
+from sklearn.metrics import accuracy_score, confusion_matrix
+
+
+# Plot settings
+plt.style.use("fivethirtyeight")
+plt.rcParams["figure.figsize"] = (20, 5)
+plt.rcParams["figure.dpi"] = 100
+plt.rcParams["lines.linewidth"] = 2
+
+df = pd.read_pickle("../../data/interim/03_data_features.pkl")
+df.columns
+
+
+# --------------------------------------------------------------
+# Create a training and test set
+# --------------------------------------------------------------
+
+df_train = df.drop(["participant", "category", "duration", "set"], axis=1)
+
+
+X = df_train.drop("label", axis=1)
+y = df_train["label"]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
+
+# --------------------------------------------------------------
+# Split feature subsets
+# --------------------------------------------------------------
+
+
+# --------------------------------------------------------------
+# Perform forward feature selection using simple decision tree
+# --------------------------------------------------------------
+
+
+# --------------------------------------------------------------
+# Grid search for best hyperparameters and model selection
+# --------------------------------------------------------------
+
+
+# --------------------------------------------------------------
+# Create a grouped bar plot to compare the results
+# --------------------------------------------------------------
+
+
+# --------------------------------------------------------------
+# Select best model and evaluate results
+# --------------------------------------------------------------
+
+
+# --------------------------------------------------------------
+# Select train and test data based on participant
+# --------------------------------------------------------------
+
+
+# --------------------------------------------------------------
+# Use best model again and evaluate results
+# --------------------------------------------------------------
+
+
+# --------------------------------------------------------------
+# Try a simpler model with the selected features
+# --------------------------------------------------------------
